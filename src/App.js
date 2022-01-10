572109c6cfae0ed7ab51fccc6a1905176520c60a
unknown <sargey7@gmail.com>
2022-01-10 15:05:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import BasicTitle from "./components/BasicTitle";
+import { DefaultButton } from "./components/Buttons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BasicTitle>Style Componenets</BasicTitle>
+      <DefaultButton>click me</DefaultButton>
     </div>
   );
 }
